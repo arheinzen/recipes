27c6f6a913ff8918db960140f10652f5de5f0668
Ammon Heinzen <Ammon@Ammons-MacBook-Pro.local>
2016-02-12 16:18:11 -0700

    heroku final debug commit

--- recipes/settings.py
+++ recipes/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'kktdt6$-=2fv7(ceo1=irq8z)rdu&##+q*1*pn)2f9_1m@4ant'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
